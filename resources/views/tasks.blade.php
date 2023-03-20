3ee1521ebe2b59fea1e5401080897d219facb633
SamiMadane <samimadane23@gmail.com>
2023-03-20 17:07:18 +0200

    second commi

---
+++ resources/views/tasks.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Tasks:</h1>
+    <ul>
+        @foreach ($tasks as $task)
+        <h3>
+            <li>
+                {{$task->name}}
+            </li>
+        </h3>
+        @endforeach
+
+    </ul>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,30 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// استدعاء الصفحة
+Route::get('about', function () {
+    $name = 'Sami';
+    return view('about', compact('name'));
+});
+
+// عند الضغط على الزر 
+Route::post('about', function () {
+    $name = 'Sami';
+    if (isset($_POST['name']))
+        $name = $_POST['name'];
+    return view('about', compact('name'));
+});
+
+/*
+Route::get('tasks', function () {
+    $tasks = ['task1','task2','task3'];
+    return view('tasks',compact('tasks'));
+});
+*/
+Route::get('tasks', function () {
+    $tasks = DB::table('tasks')->get();
+   // return $tasks;
+    return view('tasks',compact('tasks'));
 });
